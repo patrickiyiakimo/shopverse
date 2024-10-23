0aa837d323fcf37c0c682713ee4cbcb2225408b9
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-23 15:53:30 +0100

    added a drawer

---
+++ components/Drawer.tsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+
+export default function Drawer() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <div className="text-center">
+        <button
+          className="mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          type="button"
+          onClick={toggleDrawer}
+        >
+          C
+        </button>
+      </div>
+
+      {/* drawer component */}
+      <div
+        id="drawer-contact"
+        className={`fixed left-0 top-0 z-40 h-screen w-80 bg-white p-4 transition-transform dark:bg-gray-800 ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+        aria-labelledby="drawer-contact-label"
+      >
+        <h5
+          id="drawer-label"
+          className="mb-6 inline-flex items-center text-base font-semibold uppercase text-gray-500 dark:text-gray-400"
+        >
+          <svg
+            className="me-2.5 h-4 w-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 16"
+          >
+            <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+            <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+          </svg>
+          Contact us
+        </h5>
+        <button
+          type="button"
+          onClick={toggleDrawer}
+          className="absolute end-2.5 top-2.5 inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
+        >
+          <svg
+            className="h-3 w-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+          <span className="sr-only">Close menu</span>
+        </button>
+        <form className="mb-6">
+          <div className="mb-6">
+            <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+              Your email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              placeholder="name@company.com"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+              Subject
+            </label>
+            <input
+              type="text"
+              id="subject"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              placeholder="Let us know how we can help you"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+              Your message
+            </label>
+            <textarea
+              id="message"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              placeholder="Your message..."
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="mb-2 block w-full rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Send message
+          </button>
+        </form>
+        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+          <a href="#" className="hover:underline">
+            info@company.com
+          </a>
+        </p>
+        <p className="text-sm text-gray-500 dark:text-gray-400">
+          <a href="#" className="hover:underline">
+            212-456-7890
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
