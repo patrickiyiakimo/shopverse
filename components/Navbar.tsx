44cec48fabdd17614122af5e6b3bc6442e7553b5
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-23 22:18:41 +0100

    created my hero section

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,30 +13,25 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
+    <main className="">
       <nav className="fixed start-0 top-0 z-20 w-full border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-900">
         <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
           <a
             href="https://flowbite.com/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
-              <span className="bg-blue-700 p-0.5 text-white ">S</span>hopVerse
-            </span>
+            <em className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+              ShopVerse
+            </em>
           </a>
 
-          <div className="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
-            {/* <button
-              type="button"
-              className="rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            > */}
+          <div className="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">       
              <Drawer />
-            {/* </button> */}
             <button
               type="button"
-              onClick={toggleMenu} // Toggle menu on click
+              onClick={toggleMenu} 
               className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
-              aria-expanded={isMenuOpen} // ARIA attribute for accessibility
+              aria-expanded={isMenuOpen} 
             >
               <span className="sr-only">Open main menu</span>
               <svg
