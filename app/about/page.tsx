d09ce0d39128b1d00c707370d2b2ba3d568a79a2
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-10-24 23:06:45 +0100

    added contents to the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,12 +2,21 @@ import React from 'react'
 
 export default function CartPage() {
   return (
-    <div className='pt-40'>
-      <div>
-        <h1>Empower Your Success,</h1>
-        <h1>Our Mission, Vision and Goal</h1>
-        <p>With a </p>
-      </div>
+    <div className="pt-40">
+      <section className="bg-white dark:bg-gray-900">
+        <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:py-16">
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-4xl">
+            we’re passionate about more than just selling products.
+          </h1>
+          <p className="mb-8 text-lg font-normal text-gray-500 dark:text-gray-400 sm:px-16 lg:px-48 lg:text-xl">
+            Our skincare solutions are crafted using the finest natural
+            ingredients, free from harmful chemicals, and designed to bring out
+            your best skin ever. Whether you’re battling dryness, sensitivity,
+            or looking to elevate your self-care routine, we’re here to help you
+            feel confident in your own skin every day.
+          </p>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
