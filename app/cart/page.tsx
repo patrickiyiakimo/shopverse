6f64d14a6ad65781ce7b205c9349be03c813650c
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-11-30 08:47:11 +0100

    changed the remove text to an actual delete icon

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import React, { useEffect, useState } from "react";
+import { MdDelete } from "react-icons/md";
+
 
 interface CartItem {
   id: number;
@@ -32,7 +34,7 @@ const CartPage: React.FC = () => {
                   <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-white">
                     <tr>
                       <th scope="col" className="px-16 py-3">
-                      Image
+                        Image
                       </th>
                       <th scope="col" className="px-6 py-3">
                         Product
@@ -55,78 +57,78 @@ const CartPage: React.FC = () => {
                           <img
                             src={item.image}
                             alt={item.name}
-                            className="max-h-full size-10 md:size-32 lg:size-16 rounded-full max-w-full md:w-32"
+                            className="size-10 max-h-full max-w-full rounded-full md:size-32 md:w-32 lg:size-16"
                           />
                         </td>
                       </td>
                       <td className="px-6 py-4 text-gray-900 dark:text-white">
-                                      {item.name}
-                                      </td>
-                        <div className="flex text-gray-950 mt-12 items-center">
-                          <button
-                            className="me-3 inline-flex h-6 w-6 items-center justify-center rounded-full border border-gray-300 bg-white p-1 text-sm font-medium text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                            type="button"
+                        {item.name}
+                      </td>
+                      <div className="mt-12 flex items-center text-gray-950">
+                        <button
+                          className="me-3 inline-flex h-6 w-6 items-center justify-center rounded-full border border-gray-300 bg-white p-1 text-sm font-medium text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                          type="button"
+                        >
+                          <span className="sr-only">Quantity button</span>
+                          <svg
+                            className="size-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 2"
                           >
-                            <span className="sr-only">Quantity button</span>
-                            <svg
-                              className="size-3"
-                              aria-hidden="true"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 18 2"
-                            >
-                              <path
-                                stroke="currentColor"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M1 1h16"
-                              />
-                            </svg>
-                          </button>
-                          <div>
-                            <input
-                              type="number"
-                              id="first_product"
-                              className="block w-14 rounded-lg border border-gray-300 bg-gray-50 px-2.5 py-1 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                              placeholder="1"
-                              required
+                            <path
+                              stroke="currentColor"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="M1 1h16"
                             />
-                          </div>
-                          <button
-                            className="ms-3 inline-flex h-6 w-6 items-center justify-center rounded-full border border-gray-300 bg-white p-1 text-sm font-medium text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                            type="button"
-                          >
-                            <span className="sr-only">Quantity button</span>
-                            <svg
-                              className="h-3 w-3"
-                              aria-hidden="true"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 18 18"
-                            >
-                              <path
-                                stroke="currentColor"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M9 1v16M1 9h16"
-                              />
-                            </svg>
-                          </button>
+                          </svg>
+                        </button>
+                        <div>
+                          <input
+                            type="number"
+                            id="first_product"
+                            className="block w-14 rounded-lg border border-gray-300 bg-gray-50 px-2.5 py-1 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                            placeholder="1"
+                            required
+                          />
                         </div>
-                      
+                        <button
+                          className="ms-3 inline-flex h-6 w-6 items-center justify-center rounded-full border border-gray-300 bg-white p-1 text-sm font-medium text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                          type="button"
+                        >
+                          <span className="sr-only">Quantity button</span>
+                          <svg
+                            className="h-3 w-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 18"
+                          >
+                            <path
+                              stroke="currentColor"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="M9 1v16M1 9h16"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+
                       <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
                         <p className="text-lg font-bold">
-                           ${item.price.toFixed(2)}
+                          ${item.price.toFixed(2)}
                         </p>
                       </td>
                       <td className="px-6 py-4">
                         <a
                           href="#"
-                          className="font-medium text-red-600 hover:underline dark:text-red-500"
+                          className="text-red-600 dark:text-red-500"
                         >
-                          Remove
+                          <MdDelete className="size-7"/>
                         </a>
                       </td>
                     </tr>
@@ -139,7 +141,7 @@ const CartPage: React.FC = () => {
           <p>No items in the cart.</p>
         )}
       </div>
-    </div >
+    </div>
   );
 };
 
