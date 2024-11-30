973f5161e8b0d6ba68b8cbcc7172129ff014cc23
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-11-30 01:26:46 +0100

    made the items added to cart to show at the cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,13 +1,146 @@
-import React from 'react'
+"use client"
 
-export default function page() {
-  return (
-    <div className="pt-40 text-6xl">
-      <div>
-        <h1 className="pb-20 text-center text-2xl font-bold md:text-4xl lg:text-6xl">
-          Items added to cart
-        </h1>
-      </div>
-    </div>
-  );
+import React, { useEffect, useState } from "react";
+
+interface CartItem {
+  id: number;
+  image: string;
+  name: string;
+  price: number;
+  quantity: number;
 }
+
+const CartPage: React.FC = () => {
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  useEffect(() => {
+    const storedCart = JSON.parse(localStorage.getItem("cart") || "[]");
+    setCart(storedCart);
+  }, []);
+
+  return (
+    <div className="pt-20">
+      <h1 className="pb-20 text-center text-2xl font-bold">Cart Items</h1>
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+        {cart.length > 0 ? (
+          cart.map((item) => (
+            //    this is the content
+
+            <div key={item.id} className="rounded border p-4">
+              <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right">
+                  <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="px-16 py-3">
+                        <span className="sr-only">Image</span>
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Product
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Qty
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Price
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Action
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
+                      <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                        <td className="p-4">
+                          <img
+                            src={item.image}
+                            alt={item.name}
+                            className="max-h-full size-16 rounded-full max-w-full md:w-32"
+                          />
+                        </td>
+                      </td>
+                      <td className="px-6 py-4">
+                                      {item.name}
+                                      </td>
+                        <div className="flex items-center">
+                          <button
+                            className="me-3 inline-flex h-6 w-6 items-center justify-center rounded-full border border-gray-300 bg-white p-1 text-sm font-medium text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                            type="button"
+                          >
+                            <span className="sr-only">Quantity button</span>
+                            <svg
+                              className="h-3 w-3"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 18 2"
+                            >
+                              <path
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M1 1h16"
+                              />
+                            </svg>
+                          </button>
+                          <div>
+                            <input
+                              type="number"
+                              id="first_product"
+                              className="block w-14 rounded-lg border border-gray-300 bg-gray-50 px-2.5 py-1 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                              placeholder="1"
+                              required
+                            />
+                          </div>
+                          <button
+                            className="ms-3 inline-flex h-6 w-6 items-center justify-center rounded-full border border-gray-300 bg-white p-1 text-sm font-medium text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                            type="button"
+                          >
+                            <span className="sr-only">Quantity button</span>
+                            <svg
+                              className="h-3 w-3"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 18 18"
+                            >
+                              <path
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M9 1v16M1 9h16"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                      
+                      <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                        <p className="text-lg font-bold">
+                          Price: ${item.price.toFixed(2)}
+                        </p>
+                      </td>
+                      <td className="px-6 py-4">
+                        <a
+                          href="#"
+                          className="font-medium text-red-600 hover:underline dark:text-red-500"
+                        >
+                          Remove
+                        </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No items in the cart.</p>
+        )}
+      </div>
+    </div >
+  );
+};
+
+export default CartPage;
--- components/BestSellingProducts.tsx
+++ components/BestSellingProducts.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
 
@@ -10,22 +10,32 @@ interface Product {
   price: number;
 }
 
+interface CartItem {
+  id: number;
+  image: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
 const fetchProducts = async (): Promise<Product[]> => {
   try {
-    const response = await fetch("skincare.json");
+    const response = await fetch("/skincare.json");
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
     const data: Product[] = await response.json();
     return data;
   } catch (error) {
-    console.log("Failed to fetch:", error);
+    console.error("Failed to fetch:", error);
     return [];
   }
 };
 
 const BestSellingProducts: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [cart, setCart] = useState<CartItem[]>([]);
+  const [showToast, setShowToast] = useState(false);
 
   useEffect(() => {
     const getProducts = async () => {
@@ -34,54 +44,107 @@ const BestSellingProducts: React.FC = () => {
     };
 
     getProducts();
+
+    // Load cart items from local storage
+    const storedCart = JSON.parse(localStorage.getItem("cart") || "[]");
+    setCart(storedCart);
   }, []);
+
+  const handleCartClick = (product: Product) => {
+    const existingCartItem = cart.find((item) => item.id === product.id);
+    if (existingCartItem) {
+      // Optionally show a message that the item is already in the cart
+    } else {
+      const newCartItem: CartItem = {
+        id: product.id,
+        image: product.image,
+        name: product.name,
+        price: product.price,
+        quantity: 1,
+      };
+      const updatedCart = [...cart, newCartItem];
+      setCart(updatedCart);
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
+      setShowToast(true);
+    }
+  };
 
   return (
     <div className="pt-20 lg:pt-32">
-      <div className="">
-        <h1 className="pb-20 text-center text-2xl font-bold md:text-4xl lg:text-6xl">
-          Best Selling Products
-        </h1>
-        <section className="mb-20 ml-20 sm:ml-24 md:ml-40 lg:ml-5 items-center justify-center lg:flex">
-          <input
-            type="text"
-            placeholder="search by description"
-            className="mb-4 mr-5 border-2 border-gray-300 bg-gray-200 py-4 placeholder:text-gray-900 md:mb-0 md:mr-20"
-          />
-          <input
-            type="text"
-            placeholder="search by name"
-            className="border-2 border-gray-300 bg-gray-200 py-4 placeholder:text-gray-900"
-          />
-        </section>
-        <div className="grid w-full  grid-cols-1 gap-4 rounded-lg border border-gray-200 bg-white shadow dark:border-gray-900 dark:bg-gray-800 md:grid-cols-2 lg:grid-cols-3">
-          {products.map((product) => (
-            <div key={product.id} className="rounded border p-4">
-              <img
-                src={product.image}
-                alt={product.name}
-                className="mb-4 h-auto max-h-60 w-full rounded"
+      {showToast && (
+        <div
+          className="mb-4 flex w-full max-w-xs items-center rounded-lg bg-white p-4 text-gray-500 shadow dark:bg-gray-800 dark:text-gray-400"
+          role="alert"
+        >
+          <div className="inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
+            <svg
+              className="h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+          </div>
+          <div className="ml-3 text-sm font-normal">Item added to cart.</div>
+          <button
+            type="button"
+            onClick={() => setShowToast(false)}
+            className="ml-auto rounded-lg bg-white p-1.5 text-gray-400 hover:bg-gray-100"
+          >
+            <svg
+              className="xmlns= h-3 w-3"
+              // http:fill="none" //www.w3.org/2000/svg"
+              viewBox="0 0 14 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                d="M1 1l6 6m0 0l6 6M7 7l6-6m-6 6l-6-6"
               />
-              <a href="#">
-                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                  {product.name}
-                </h5>
-              </a>
-              <p>{product.description}</p>
-              <div className="flex items-center justify-between">
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                  <p className="font-bold">${product.price.toFixed(2)}</p>
-                </span>
-                <a
-                  href="#"
-                  className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Add to cart
-                </a>
-              </div>
-            </div>
-          ))}
+            </svg>
+          </button>
         </div>
+      )}
+      <h1 className="pb-20 text-center text-2xl font-bold">
+        Best Selling Products
+      </h1>
+      <section className="mb-20 ml-20 items-center justify-center sm:ml-24 md:ml-40 lg:ml-5 lg:flex">
+           <input
+             type="text"
+             placeholder="search by description"
+             className="mb-4 mr-5 border-2 border-gray-300 bg-gray-200 py-4 placeholder:text-gray-900 md:mb-0 md:mr-20"
+           />
+           <input
+             type="text"
+             placeholder="search by name"
+             className="border-2 border-gray-300 bg-gray-200 py-4 placeholder:text-gray-900"
+           />
+         </section>
+      <div className="grid w-full grid-cols-1 gap-4 rounded-lg border border-gray-200 bg-white shadow dark:border-gray-900 dark:bg-gray-800 md:grid-cols-2 lg:grid-cols-3">
+        {products.map((product) => (
+          <div key={product.id} className="rounded border p-4">
+            <img
+              src={product.image}
+              alt={product.name}
+              className="mb-4 h-auto max-h-60 w-full rounded"
+            />
+            <h5 className="text-xl font-semibold">{product.name}</h5>
+            <p>{product.description}</p>
+            <div className="flex items-center justify-between">
+              <span className="text-3xl font-bold">
+                ${product.price.toFixed(2)}
+              </span>
+              <button
+                onClick={() => handleCartClick(product)}
+                className="rounded bg-blue-700 px-5 py-2 text-white hover:bg-blue-800"
+              >
+                Add to cart
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
