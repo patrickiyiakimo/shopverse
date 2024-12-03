4a6df7897f712b4f0b0f6e6d1b3acbc6055e9d03
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-12-03 12:01:55 +0100

    added a transition to the image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -62,7 +62,7 @@ export default function Hero() {
               />
               <button
                 type="submit"
-                className="absolute bottom-2.5 end-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="absolute bottom-2.5 end-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 transition focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Sign up
               </button>
