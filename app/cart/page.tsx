b0617471ee0ab53b04f354816662d81b95976714
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-12-03 17:29:35 +0100

    made some changes

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { MdDelete } from "react-icons/md";
- import { IoCart } from "react-icons/io5";
+import { IoCart } from "react-icons/io5";
 
 interface CartItem {
   id: number;
