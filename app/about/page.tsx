8e7ad05cfced3801eba2e259d67436e23deaf7c4
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-12-04 00:30:42 +0100

    builf the avout page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,7 +17,7 @@ export default function CartPage() {
           </p>
         </div>
       </section>
-      <div className="mr-5 ml-5  grid grid-cols-2 gap-4 md:grid-cols-4 md:mx-20 lg:mx-20">
+      <div className="ml-5 mr-5  grid grid-cols-2 gap-4 md:mx-20 md:grid-cols-4 lg:mx-20">
         <div className="grid gap-4">
           <div>
             <img
@@ -111,10 +111,35 @@ export default function CartPage() {
           </div>
         </div>
       </div>
-      <section className="bg-blue-800 mt-10 mx-5 md:mx-20 rounded-lg px-10 py-10 md:px-32 md:py-20">
-        <div className="md:flex">
-          <h3 className="font-mont font-bold text-white text-xl md:text-2xl lg:text-4xl">Perform Market Research to gain Insights into Industry Trends</h3>
-          <button>Get Started Now</button>
+      <section className="mx-5 mt-10 rounded-lg bg-blue-800 px-10 py-10 md:mx-20  md:px-32 md:py-20">
+        <div className="">
+          <h3 className="font-mont mr-10 text-xl font-bold text-white md:text-2xl lg:text-4xl">
+            Perform Market Research to gain Insights into Industry Trends
+          </h3>
+          {/* <button className="whitespace-nowrap bg-blue-400 px-2 rounded-full">
+            Get Started Now
+          </button> */}
+          <div className="flex items-center mt-5 text-white dark:text-white">
+            <svg
+              className="me-1 size-4 text-yellow-300"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+            <p className="ms-2 text-sm font-bold dark:text-white">
+              4.95
+            </p>
+            <span className="mx-1.5 size-1 rounded-full bg-gray-500 dark:bg-gray-400"></span>
+            <a
+              href="#"
+              className="text-sm font-mediu underline hover:no-underline dark:text-white"
+            >
+              73 reviews
+            </a>
+          </div>
         </div>
       </section>
     </div>
