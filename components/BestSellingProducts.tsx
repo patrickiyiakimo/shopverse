e9f8c3c74572139165853380abea03b42f0cad02
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-12-03 11:43:23 +0100

    added a transition to the add to cart button

--- components/BestSellingProducts.tsx
+++ components/BestSellingProducts.tsx
@@ -172,7 +172,7 @@ const BestSellingProducts: React.FC = () => {
               ) : (
                 <button
                   onClick={() => handleCartClick(product)}
-                  className="rounded bg-blue-700 px-5 py-2 text-white hover:bg-blue-800"
+                  className="rounded bg-blue-700 px-5 py-2 text-white hover:bg-blue-800 transition"
                 >
                   Add to cart
                 </button>
