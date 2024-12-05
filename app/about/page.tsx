1590284dcd4b5ebe159dafa77e4001cd1d204d6e
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-12-05 09:48:00 +0100

    made it responsive to all screen sizes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -113,10 +113,10 @@ export default function CartPage() {
       </div>
       <section className="mx-5 mt-10 rounded-lg bg-blue-800 px-10 py-10 md:mx-20  md:px-32 md:py-20">
         <div className="">
-          <h3 className="font-mont mr-10 text-xl font-bold text-white md:text-2xl lg:text-4xl">
+          <h3 className="mr-10 font-mont text-xl font-bold text-white md:text-2xl lg:text-4xl">
             Perform Market Research to gain Insights into Industry Trends
           </h3>
-          <div className="flex items-center mt-5 text-white dark:text-white">
+          <div className="mt-5 flex items-center text-white dark:text-white">
             <svg
               className="me-1 size-4 text-yellow-300"
               aria-hidden="true"
@@ -126,19 +126,31 @@ export default function CartPage() {
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
-            <p className="ms-2 text-sm font-bold dark:text-white">
-              4.95
-            </p>
+            <p className="ms-2 text-sm font-bold dark:text-white">4.95</p>
             <span className="mx-1.5 size-1 rounded-full bg-gray-500 dark:bg-gray-400"></span>
             <a
               href="#"
-              className="text-sm font-mediu underline hover:no-underline dark:text-white"
+              className="font-mediu text-sm underline hover:no-underline dark:text-white"
             >
               73 reviews
             </a>
           </div>
         </div>
-        <hr className="mt-2"/>
+        <hr className="mb-2 mt-2" />
+        <div className="font-mont lg:flex">
+          <span className="flex uppercase">
+            <p className="mr-2 text-xl md:text-3xl lg:text-5xl">12K+</p>
+            <p className="mr-3 mt-1 text-sm md:text-[15px]">total projects completed</p>
+          </span>
+          <span className="flex uppercase">
+            <p className="mr-2 text-xl md:text-3xl lg:text-5xl">300K+</p>
+            <p className="mr-3 mt-1 text-sm md:text-[15px]">satisfied active customers</p>
+          </span>
+          <span className="flex uppercase">
+            <p className="mr-2 text-xl md:text-3xl lg:text-5xl">4.95</p>
+            <p className="mr-3 mt-1 text-sm md:text-[15px]">active clients rating</p>
+          </span>
+        </div>
       </section>
     </div>
   );
