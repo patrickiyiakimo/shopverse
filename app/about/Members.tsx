3dbbc95fb6076c6217a8153bde8978a708c34c41
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-12-06 22:57:29 +0100

    added some more content

--- app/about/Members.tsx
+++ app/about/Members.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 export default function Members() {
   return (
       <div>
-          <div className='text-center text-xl md:text2xl lg:text-4xl font-mont font-semibold pt-10'>
+          <div className='text-center text-xl md:text2xl lg:text-4xl font-mont font-semibold pt-40'>
               <h3>We have 50+ Active Dedicated Members for Helping the Customers</h3>
+              <p className='text-xl px-20 py-1'>Globally engage cross-media leadership skills before cross-media innovation develop standardised platforms without robust applications, Convieniently go forward collaboration and idea-sharing.</p>
           </div>
     </div>
   )
