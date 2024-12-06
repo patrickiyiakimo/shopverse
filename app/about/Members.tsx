e2d88e948613acc285a771389f321c229cdebff9
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-12-06 23:04:09 +0100

    changed the font sixe

--- app/about/Members.tsx
+++ app/about/Members.tsx
@@ -5,7 +5,7 @@ export default function Members() {
       <div>
           <div className='text-center text-xl md:text2xl lg:text-4xl font-mont font-semibold pt-40'>
               <h3>We have 50+ Active Dedicated Members for Helping the Customers</h3>
-              <p className='text-xl px-20 py-1'>Globally engage cross-media leadership skills before cross-media innovation develop standardised platforms without robust applications, Convieniently go forward collaboration and idea-sharing.</p>
+              <p className='text-xl px-20 py-1 font-normal'>Globally engage cross-media leadership skills before cross-media innovation develop standardised platforms without robust applications, Convieniently go forward collaboration and idea-sharing.</p>
           </div>
     </div>
   )
