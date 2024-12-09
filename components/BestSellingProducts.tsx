c8dab38939939a4fcfb7609d4491ce05e3d7e15f
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-12-09 18:53:42 +0100

    made some extra changes

--- components/BestSellingProducts.tsx
+++ components/BestSellingProducts.tsx
@@ -102,7 +102,7 @@ const BestSellingProducts: React.FC = () => {
             key={product.id}
             className="rounded border p-4 dark:border-gray-600"
           >
-            <FaHeart className="absolute ml-5 mt-5 size-5" />
+            <FaHeart className="absolute ml-5 mt-5 size-5 text-green-900" />
             <img
               src={product.image}
               alt={product.name}
