446bed2d418604e5da1ffa70235f1d8202075618
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-12-31 17:09:03 +0100

    created the favourites page

---
+++ app/favourites/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+      <div className='min-h-screen font-mont'>
+          <div className='pt-40'>
+              <h1>Favourite Products</h1>
+          </div>
+    </div>
+  )
+}
