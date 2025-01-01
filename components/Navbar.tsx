d6338e9c9b9094bd9c8a1d1b5ddbe2432d46f185
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2025-01-01 18:14:04 +0100

    got rid of an unnecessary comment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -111,7 +111,7 @@ export default function Home() {
                 </li>
               </Link>
 
-              <div className="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
+              <div className="flex space-x-3 ml-2 md:order-2 md:space-x-0 rtl:space-x-reverse">
                 <RightDrawer />
                 <button
                   type="button"
@@ -119,22 +119,6 @@ export default function Home() {
                   className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
                   aria-expanded={isMenuOpen}
                 >
-                  <span className="sr-only">Open main menu</span>
-                  <svg
-                    className="h-5 w-5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 17 14"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M1 1h15M1 7h15M1 13h15"
-                    />
-                  </svg>
                 </button>
               </div>
 
