c0b85a09b2b5d883394b2f1f09b077c531c9fd5a
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2025-01-02 16:32:58 +0100

    made the signup border more visible

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -29,7 +29,7 @@ export default function page() {
                 type="text"
                 id="name"
                 required
-                className="mt-1 block w-full rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500"
+                className="mt-1 block w-full rounded-md border border-gray-500 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500"
                 placeholder="Enter your name"
               />
             </div>
@@ -44,7 +44,7 @@ export default function page() {
                 type="email"
                 id="email"
                 required
-                className="mt-1 block w-full rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500"
+                className="mt-1 block w-full rounded-md border border-gray-500 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500"
                 placeholder="Enter your email"
               />
             </div>
@@ -59,7 +59,7 @@ export default function page() {
                 type="password"
                 id="password"
                 required
-                className="mt-1 block w-full rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500"
+                className="mt-1 block w-full rounded-md border border-gray-500 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500"
                 placeholder="Enter your password"
               />
             </div>
