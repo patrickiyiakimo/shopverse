aebe6d287f33ae01e64f5ec018052497a5d78a0f
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2025-01-07 16:59:34 +0100

    changed the opacity to the latest format

--- components/RightDrawer.tsx
+++ components/RightDrawer.tsx
@@ -33,7 +33,7 @@ export default function RightDrawer() {
 
       {isOpen && (
         <div
-          className="fixed inset-0 z-40 bg-black bg-opacity-50"
+          className="fixed inset-0 z-40 bg-black/50"
           onClick={toggleDrawer}
         ></div>
       )}
