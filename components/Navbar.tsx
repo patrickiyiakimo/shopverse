84dd2955e5a1276db1d4948c8fb82258c0d09f5e
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2025-01-27 15:27:17 +0100

    changed the menu icon color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Home() {
             <button
               type="button"
               onClick={toggleMenu}
-              className="inline-flex size-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+              className="inline-flex size-10 items-center justify-center rounded-lg p-2 text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
               aria-expanded={isMenuOpen}
             >
               <span className="sr-only">Open main menu</span>
