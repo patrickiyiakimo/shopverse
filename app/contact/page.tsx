858afbebd6c7ad6514c0f1aa6a3350834c490461
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2025-01-29 16:31:01 +0100

    make a file change

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
               id="name"
               required
               className="mt-1 block w-full rounded-md border border-gray-500 bg-white px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 dark:bg-gray-700"
-              placeholder="Enter your name"
+              placeholder="Enter your "
             />
           </div>
           <div className="mb-4">
