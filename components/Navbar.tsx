15db431062b1caf9315ebd7fc1767c3035c0f769
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2025-02-19 12:33:59 +0100

    removed the home from the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -48,18 +48,6 @@ export default function Home() {
             id="navbar-sticky"
           >
             <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900 rtl:space-x-reverse">
-              <Link href="/">
-                <li
-                  className={`mt-3 block rounded px-3 py-2 ${
-                    isActive("/")
-                      ? "bg-blue-700 text-white lg:bg-auto lg:text-blue-600"
-                      : "text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white"
-                  } md:bg-transparent md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500`}
-                  aria-current={isActive("/") ? "page" : undefined}
-                >
-                  Home
-                </li>
-              </Link>
 
               <Link href="/about">
                 <li
