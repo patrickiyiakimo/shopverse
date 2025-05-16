95faa8242df7bbda654f506259eb13999d23e611
Patrick Iyiakimo <137924636+patrickiyiakimo@users.noreply.github.com>
2025-05-16 18:50:26 +0100

    feat: made some changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
             Sign Up
           </h2>
           <div className="mb-4">
-            <label
+            {/* <label
               htmlFor="name"
               className="block text-sm font-medium text-gray-700 dark:text-white"
             >
@@ -23,7 +23,7 @@ export default function Page() {
               className="mt-1 block w-full rounded-md border border-gray-500 bg-white px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 dark:bg-gray-700"
               placeholder="Enter your name"
             />
-          </div>
+          </div> */}
           <div className="mb-4">
             <label
               htmlFor="name"
